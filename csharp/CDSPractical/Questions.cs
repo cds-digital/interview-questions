251f5583c0ad5122e47034185c4e89521d7933e5
Philip Skinner <philip.skinner@cds.co.uk>
2019-01-17 10:55:56 +0000

    initial c# practical questions added

---
+++ csharp/CDSPractical/Questions.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CDSPractical {
+    public class Questions {
+        /// <summary>
+        /// Given an enumerable of strings, attempt to parse each string and if 
+        /// it is an integer, add it to the returned enumerable.
+        /// 
+        /// For example:
+        /// 
+        /// ExtractNumbers(new List<string> { "123", "hello", "234" });
+        /// 
+        /// ; would return:
+        /// 
+        /// {
+        ///   123,
+        ///   234
+        /// }
+        /// </summary>
+        /// <param name="source">An enumerable containing words</param>
+        /// <returns></returns>
+        public IEnumerable<int> ExtractNumbers(IEnumerable<string> source) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Given two enumerables of strings, find the longest common word.
+        /// 
+        /// For example:
+        /// 
+        /// LongestCommonWord(
+        ///     new List<string> {
+        ///         "love",
+        ///         "wandering",
+        ///         "goofy",
+        ///         "sweet",
+        ///         "mean",
+        ///         "show",
+        ///         "fade",
+        ///         "scissors",
+        ///         "shoes",
+        ///         "gainful",
+        ///         "wind",
+        ///         "warn"
+        ///     },
+        ///     new List<string> {
+        ///         "wacky",
+        ///         "fabulous",
+        ///         "arm",
+        ///         "rabbit",
+        ///         "force",
+        ///         "wandering",
+        ///         "scissors",
+        ///         "fair",
+        ///         "homely",
+        ///         "wiggly",
+        ///         "thankful",
+        ///         "ear"
+        ///     }
+        /// );
+        /// 
+        /// ; would return "wandering" as the longest common word.
+        /// </summary>
+        /// <param name="first">First list of words</param>
+        /// <param name="second">Second list of words</param>
+        /// <returns></returns>
+        public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that converts kilometers to miles, given that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInMiles(16.00);
+        /// 
+        /// ; would return 10.00;
+        /// </summary>
+        /// <param name="km">distance in kilometers</param>
+        /// <returns></returns>
+        public double DistanceInMiles(double km) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that converts miles to kilometers, give that there are
+        /// 1.6 kilometers per mile.
+        /// 
+        /// For example:
+        /// 
+        /// DistanceInKm(10.00);
+        /// 
+        /// ; would return 16.00;
+        /// </summary>
+        /// <param name="miles">distance in miles</param>
+        /// <returns></returns>
+        public double DistanceInKm(double miles) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write a method that returns true if the word is a palindrome, false if
+        /// it is not.
+        /// 
+        /// For example:
+        /// 
+        /// IsPalindrome("bolton");
+        /// 
+        /// ; would return false, and:
+        /// 
+        /// IsPalindrome("Anna");
+        /// 
+        /// ; would return true.
+        /// 
+        /// Also complete the related test case for this method.
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns></returns>
+        public bool IsPalindrome(string word) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate a list of integers from 1 to 100.
+        /// 
+        /// This method is currently broken, fix it so that the tests pass.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> GenerateList() {
+            var ret = new List<int>();
+            var numThreads = 2;
+
+            Thread[] threads = new Thread[numThreads];
+            for (var i = 0; i < numThreads; i++) {
+                threads[i] = new Thread(() => {
+                    var complete = false;
+                    while (!complete) {                        
+                        var next = ret.Count + 1;
+                        Thread.Sleep(new Random().Next(1, 10));
+                        if (next <= 100) {
+                            ret.Add(next);
+                        }
+
+                        if (ret.Count >= 100) {
+                            complete = true;
+                        }
+                    }                    
+                });
+                threads[i].Start();
+            }
+
+            for (var i = 0; i < numThreads; i++) {
+                threads[i].Join();
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ csharp/CDSPracticalTests/Tests.cs
@@ -0,0 +1,111 @@
+using CDSPractical;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CDSPracticalTests {
+    public class Tests {
+        Questions instance = new Questions();
+
+        [Fact]
+        public void CanExtractNumbers() {
+            Assert.Equal(new List<int> {
+                123,
+                234
+            }, instance.ExtractNumbers(new List<string> {
+                "123",
+                "hello",
+                "234"
+            }));
+
+            Assert.Equal(new List<int> {}, instance.ExtractNumbers(new List<string> {                
+                "hello",
+                "there"
+            }));
+
+            Assert.Equal(new List<int> {
+                123,
+                345
+            }, instance.ExtractNumbers(new List<string> {
+                "hello",
+                "there",
+                "123",
+                "234.23",
+                "345"
+            }));
+        }
+
+        [Fact]
+        public void CanGetLongestCommonWord() {
+            Assert.Equal("wandering", instance.LongestCommonWord(
+                new List<string> {
+                    "love",
+                    "wandering",
+                    "goofy",
+                    "sweet",
+                    "mean",
+                    "show",
+                    "fade",
+                    "scissors",
+                    "shoes",
+                    "gainful",
+                    "wind",
+                    "warn"
+                },
+                new List<string> {
+                    "wacky",
+                    "fabulous",
+                    "arm",
+                    "rabbit",
+                    "force",
+                    "wandering",
+                    "scissors",
+                    "fair",
+                    "homely",
+                    "wiggly",
+                    "thankful",
+                    "ear"
+                })
+            );
+        }
+
+        [Fact]
+        public void CanGetDistanceInMiles() {
+            Assert.Equal(10.00, instance.DistanceInMiles(16.00));
+        }
+
+        [Fact]
+        public void CanGetDistanceInKilometers() {
+            Assert.Equal(16.00, instance.DistanceInKm(10.00));
+        }
+
+        [Fact]
+        public void IsPalindrome() {
+            var palindromes = new List<string> {
+                
+            };
+            var invalid = new List<string> {
+                
+            };
+
+            foreach (var word in palindromes) {
+                Assert.True(instance.IsPalindrome(word));
+            }
+
+            foreach (var word in invalid) {
+                Assert.False(instance.IsPalindrome(word));
+            }
+        }
+
+        [Fact]
+        public void CanGenerateListOfNumbers() {
+            var list = instance.GenerateList();
+
+            var current = 1;
+            foreach (var num in list) {
+                Assert.Equal(current, num);
+                current++;
+            }
+        }
+    }
+}
