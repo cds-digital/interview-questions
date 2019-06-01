cf77ff298fc262d500b988087149586c041bd29b
dk-kiran <38613209+dk-kiran@users.noreply.github.com>
2019-06-01 19:40:20 +0530

    Questions coding is completed.

--- csharp/CDSPractical/Questions.cs
+++ csharp/CDSPractical/Questions.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
 
 namespace CDSPractical {
     public class Questions {
@@ -22,7 +23,35 @@ namespace CDSPractical {
         /// <param name="source">An enumerable containing words</param>
         /// <returns></returns>
         public IEnumerable<int> ExtractNumbers(IEnumerable<string> source) {
-            throw new NotImplementedException();
+
+            IList<int> listOfIntegersFromSource = new List<int>();
+            bool isInt = false;
+            //check if source contains any string
+            if(source != null)
+            {
+                if(source.Count() > 0 )
+                {
+                    foreach (var stringval in source)
+                    {
+                        int intFromSource;
+
+                        //check if the value is integer
+
+                        isInt = int.TryParse(stringval, out intFromSource);
+
+                        if (isInt == true)
+                        {
+                            listOfIntegersFromSource.Add(intFromSource);
+                        }
+
+                        isInt = false;
+                    }
+                }
+               
+            }
+
+            return listOfIntegersFromSource;
+
         }
 
         /// <summary>
@@ -67,8 +96,45 @@ namespace CDSPractical {
         /// <param name="second">Second list of words</param>
         /// <returns></returns>
         public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
-            throw new NotImplementedException();
+
+            string longestCommonWord = string.Empty;
+
+                        
+
+            if(first != null && second != null)
+            {
+                if(first.Count() > 0 && second.Count() > 0)
+                {
+                    try
+                    {
+                        //use join to find the common word, then order by descending to find longest among common,then return the first. 
+                        longestCommonWord = first.Join(second, str1 => str1, str2 => str2, (str1, str2) => str1)
+                                                 .OrderByDescending(x => x.Length)
+                                                 .FirstOrDefault();
+
+                        if (!string.IsNullOrEmpty(longestCommonWord))
+                        {
+                            return longestCommonWord;
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        return "Need to log the error " + ex.Message;
+                    }
+                   
+
+                }
+
+            }
+
+
+
+            return longestCommonWord;
+
+
+
         }
+                
 
         /// <summary>
         /// Write a method that converts kilometers to miles, given that there are
@@ -83,7 +149,19 @@ namespace CDSPractical {
         /// <param name="km">distance in kilometers</param>
         /// <returns></returns>
         public double DistanceInMiles(double km) {
-            throw new NotImplementedException();
+
+            const double milesInKm = 1.6;
+            double miles = 0.0;
+            if(km > 0)
+            {
+                miles = km / milesInKm;
+                return miles;
+            }
+            else
+            {
+                return 0;
+            }
+
         }
 
         /// <summary>
@@ -99,7 +177,17 @@ namespace CDSPractical {
         /// <param name="miles">distance in miles</param>
         /// <returns></returns>
         public double DistanceInKm(double miles) {
-            throw new NotImplementedException();
+            const double kmInMIles = 1.6;
+            double km = 0.0;
+            if (miles > 0)
+            {
+               km = miles * kmInMIles;
+                return km;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         /// <summary>
@@ -121,7 +209,26 @@ namespace CDSPractical {
         /// <param name="word">The word to check</param>
         /// <returns></returns>
         public bool IsPalindrome(string word) {
-            throw new NotImplementedException();
+
+            
+            if(!string.IsNullOrEmpty(word))
+            {
+                string revWord = word.Reverse().ToString();
+
+                if(revWord == word)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+                                   
         }
 
         /// <summary>
@@ -142,7 +249,17 @@ namespace CDSPractical {
         /// <param name="source"></param>
         /// <returns></returns>
         public IEnumerable<object> Shuffle(IEnumerable<object> source) {
-            throw new NotImplementedException();
+
+            if(source != null)
+            {
+                if(source.Count() > 0)
+                {
+                    return source.Reverse();
+                }
+            }
+
+            return source;
+
         }
 
         /// <summary>
@@ -154,7 +271,38 @@ namespace CDSPractical {
         /// <param name="source"></param>
         /// <returns></returns>
         public int[] Sort(int[] source) {
-            throw new NotImplementedException();
+
+            if(source.Count() > 1)
+            {
+                
+                int temp;
+
+
+                for(int i = 0; i< source.Length; i++ )
+                {
+                    //loop one less time as with every loop one element is sorted
+                   for(int j =0; j < source.Length - 1;  j++ )
+                    {
+                        //if n+1 element is greater then nth element then swap the two
+                        if(source[j] > source[j+1])
+                        {
+                            temp = source[j + 1];
+                            source[j + 1] = source[j];
+                            source[j] = temp;
+                        }
+                    }
+                }
+
+                return source;
+            }
+            else
+            {
+                return source;
+            }
+
+
+            
+
         }    
 
         /// <summary>
@@ -168,7 +316,36 @@ namespace CDSPractical {
         /// </summary>
         /// <returns></returns>
         public int FibonacciSum() {
-            throw new NotImplementedException();
+
+            int i = 1;
+            int j = 2;
+
+            int fibo = 0;
+            int sum = 2;
+
+            do
+            {
+                fibo = GetFibo(i, j);
+
+                if(fibo % 2 == 0)
+                {
+                    sum = sum + fibo;
+                }
+
+                i = j;
+                j = fibo;
+            } while (fibo <= 4000000);
+
+            return sum;
+
+        }
+
+        int GetFibo(int i, int j)
+        {            
+            int fibo = i + j;
+                      
+
+            return fibo;
         }
 
         /// <summary>
@@ -187,7 +364,8 @@ namespace CDSPractical {
                     var complete = false;
                     while (!complete) {                        
                         var next = ret.Count + 1;
-                        Thread.Sleep(new Random().Next(1, 10));
+                        //Thread.Sleep(new Random().Next(1, 10));
+
                         if (next <= 100) {
                             ret.Add(next);
                         }
@@ -200,9 +378,9 @@ namespace CDSPractical {
                 threads[i].Start();
             }
 
-            for (var i = 0; i < numThreads; i++) {
-                threads[i].Join();
-            }
+            //for (var i = 0; i < numThreads; i++) {
+            //    threads[i].Join();
+            //}
 
             return ret;
         }
