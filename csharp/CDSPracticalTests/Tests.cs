ac2b71c23917dd8f2602ce1473fc8ae011f0cf9a
Ruslan Minasian <rminasian@outlook.com>
2019-07-17 16:05:56 +0300

    C# Test

--- csharp/CDSPracticalTests/Tests.cs
+++ csharp/CDSPracticalTests/Tests.cs
@@ -3,12 +3,15 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
-namespace CDSPracticalTests {
-    public class Tests {
+namespace CDSPracticalTests
+{
+    public class Tests
+    {
         Questions instance = new Questions();
 
         [Fact]
-        public void CanExtractNumbers() {
+        public void CanExtractNumbers()
+        {
             Assert.Equal(new List<int> {
                 123,
                 234
@@ -18,7 +21,7 @@ namespace CDSPracticalTests {
                 "234"
             }));
 
-            Assert.Equal(new List<int> {}, instance.ExtractNumbers(new List<string> {                
+            Assert.Equal(new List<int> { }, instance.ExtractNumbers(new List<string> {
                 "hello",
                 "there"
             }));
@@ -36,7 +39,8 @@ namespace CDSPracticalTests {
         }
 
         [Fact]
-        public void CanGetLongestCommonWord() {
+        public void CanGetLongestCommonWord()
+        {
             Assert.Equal("wandering", instance.LongestCommonWord(
                 new List<string> {
                     "love",
@@ -70,54 +74,71 @@ namespace CDSPracticalTests {
         }
 
         [Fact]
-        public void CanGetDistanceInMiles() {
+        public void CanGetDistanceInMiles()
+        {
             Assert.Equal(10.00, instance.DistanceInMiles(16.00));
         }
 
         [Fact]
-        public void CanGetDistanceInKilometers() {
+        public void CanGetDistanceInKilometers()
+        {
             Assert.Equal(16.00, instance.DistanceInKm(10.00));
         }
 
         [Fact]
-        public void IsPalindrome() {
+        public void IsPalindrome()
+        {
             var palindromes = new List<string> {
-                
+                "Anna",
+                "Kayak",
+                "Racecar"
             };
             var invalid = new List<string> {
-                
+                "bolton",
+                string.Empty,
+                null
             };
 
-            foreach (var word in palindromes) {
+            foreach (var word in palindromes)
+            {
                 Assert.True(instance.IsPalindrome(word));
             }
 
-            foreach (var word in invalid) {
+            foreach (var word in invalid)
+            {
                 Assert.False(instance.IsPalindrome(word));
             }
         }
 
         [Fact]
-        public void CanShuffle() {
+        public void CanShuffle()
+        {
             Assert.Equal(new List<string> { "two", "one" }, instance.Shuffle(new List<string> { "one", "two" }));
+            Assert.Equal(new string[0], instance.Shuffle(new string[0]));
         }
 
         [Fact]
-        public void CanSort() {
-            throw new NotImplementedException();
+        public void CanSort()
+        {
+            Assert.Equal(new[] { 1, 2, 3 }, instance.Sort(new[] { 3, 2, 1 }));
+            Assert.Equal(new[] { 1, 1, 2, 2 }, instance.Sort(new[] { 1, 2, 2, 1 }));
+            Assert.Equal(new int[0], instance.Sort(new int[0]));
         }
 
         [Fact]
-        public void CanSumFibonacciNumbers() {
+        public void CanSumFibonacciNumbers()
+        {
             Assert.Equal(4613732, instance.FibonacciSum());
         }
 
         [Fact]
-        public void CanGenerateListOfNumbers() {
+        public void CanGenerateListOfNumbers()
+        {
             var list = instance.GenerateList();
 
             var current = 1;
-            foreach (var num in list) {
+            foreach (var num in list)
+            {
                 Assert.Equal(current, num);
                 current++;
             }
