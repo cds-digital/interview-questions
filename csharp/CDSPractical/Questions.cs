067522e5ea320eb04b3acb5a28cf6879d9c4799e
Tahir Naveed <tahir.naveed@thegymgroup.com>
2020-07-30 10:50:08 +0100

    Q/A

--- csharp/CDSPractical/Questions.cs
+++ csharp/CDSPractical/Questions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CDSPractical {
     public class Questions {
@@ -22,9 +25,14 @@ namespace CDSPractical {
         /// <param name="source">An enumerable containing words</param>
         /// <returns></returns>
         public IEnumerable<int> ExtractNumbers(IEnumerable<string> source) {
-            throw new NotImplementedException();
-        }
 
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            int intValue = 0;
+            return source.Where(s => int.TryParse(s, out intValue)).Select(s => intValue);
+        }
+         
         /// <summary>
         /// Given two enumerables of strings, find the longest common word.
         /// 
@@ -67,7 +75,22 @@ namespace CDSPractical {
         /// <param name="second">Second list of words</param>
         /// <returns></returns>
         public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
-            throw new NotImplementedException();
+
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+
+            var hashFirst = new HashSet<string>(first);
+            var hashSecond = new HashSet<string>(second);
+
+            hashFirst.IntersectWith(hashSecond);
+            return hashFirst.OrderByDescending(x => x.Length)
+                .FirstOrDefault();
+            // or
+            //return first.Intersect(second, StringComparer.OrdinalIgnoreCase)
+            //    .OrderByDescending(x => x.Length)
+            //    .FirstOrDefault();
         }
 
         /// <summary>
@@ -83,7 +106,7 @@ namespace CDSPractical {
         /// <param name="km">distance in kilometers</param>
         /// <returns></returns>
         public double DistanceInMiles(double km) {
-            throw new NotImplementedException();
+            return km / 1.6;
         }
 
         /// <summary>
@@ -99,7 +122,7 @@ namespace CDSPractical {
         /// <param name="miles">distance in miles</param>
         /// <returns></returns>
         public double DistanceInKm(double miles) {
-            throw new NotImplementedException();
+            return miles * 1.6;
         }
 
         /// <summary>
@@ -121,7 +144,17 @@ namespace CDSPractical {
         /// <param name="word">The word to check</param>
         /// <returns></returns>
         public bool IsPalindrome(string word) {
-            throw new NotImplementedException();
+
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                return false;
+            }
+
+            var reversed = new string(word.Reverse().ToArray());
+            var palindrome = string.Equals(word, reversed, 
+                StringComparison.InvariantCultureIgnoreCase);
+
+            return palindrome;
         }
 
         /// <summary>
@@ -142,7 +175,16 @@ namespace CDSPractical {
         /// <param name="source"></param>
         /// <returns></returns>
         public IEnumerable<object> Shuffle(IEnumerable<object> source) {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+           
+            var rnd = new Random();
+            var result = source.Select(x => new {value = x, order = rnd.Next()})
+                .OrderBy(x => x.order).Select(x => x.value).ToList();
+
+            return result;
         }
 
         /// <summary>
@@ -153,8 +195,10 @@ namespace CDSPractical {
         /// </summary>
         /// <param name="source"></param>
         /// <returns></returns>
-        public int[] Sort(int[] source) {
-            throw new NotImplementedException();
+        public int[] Sort(int[] source)
+        {
+            source.Insertsort();
+             return source;
         }    
 
         /// <summary>
@@ -167,9 +211,11 @@ namespace CDSPractical {
         /// not exceed four million, find the sum of the even-valued terms.
         /// </summary>
         /// <returns></returns>
-        public int FibonacciSum() {
-            throw new NotImplementedException();
+        public int FibonacciSum(int limit)
+        {
+            return Util.EvenFibonacciSum(limit);
         }
+
 
         /// <summary>
         /// Generate a list of integers from 1 to 100.
@@ -177,34 +223,121 @@ namespace CDSPractical {
         /// This method is currently broken, fix it so that the tests pass.
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<int> GenerateList() {
-            var ret = new List<int>();
+        public IEnumerable<int> GenerateList()
+        {
+
+            var ret = new int[100];
             var numThreads = 2;
 
-            Thread[] threads = new Thread[numThreads];
-            for (var i = 0; i < numThreads; i++) {
-                threads[i] = new Thread(() => {
-                    var complete = false;
-                    while (!complete) {                        
-                        var next = ret.Count + 1;
-                        Thread.Sleep(new Random().Next(1, 10));
-                        if (next <= 100) {
-                            ret.Add(next);
-                        }
+            // NOTE This method is implemented wrongly based on the description. 
+            // Bare minimum:
+            //      Thread.Sleep should not be used in this situation
+            //      We need a thread lock and sync mechanism
+            // It will need a further discussion to clarify requirements
+            // before it can be fixed
 
-                        if (ret.Count >= 100) {
-                            complete = true;
-                        }
-                    }                    
+            // one way to implement in a simple manner is 
+            Parallel.For(0, 100,
+                new ParallelOptions { MaxDegreeOfParallelism = numThreads },
+                index =>
+                {
+                    // Every thread has its own item. No shared resource, no lock required.
+                    ret[index] = index+1;
                 });
-                threads[i].Start();
-            }
-
-            for (var i = 0; i < numThreads; i++) {
-                threads[i].Join();
-            }
-
             return ret;
+
+
+
+            //var ret = new List<int>();
+            //Thread[] threads = new Thread[numThreads];
+            //for (var i = 0; i < numThreads; i++)
+            //{
+            //    threads[i] = new Thread(() => {
+            //        var complete = false;
+            //        while (!complete)
+            //        {
+            //            var next = ret.Count + 1;
+            //            //Thread.Sleep(new Random().Next(1, 10));
+            //            if (next <= 100)
+            //            {
+            //                ret.Add(next);
+            //            }
+
+            //            if (ret.Count >= 100)
+            //            {
+            //                complete = true;
+            //            }
+            //        }
+            //    });
+            //    threads[i].Start();
+            //}
+
+            //for (var i = 0; i < numThreads; i++)
+            //{
+            //    threads[i].Join();
+            //}
+
+            //return ret;
+        }
+
+
+        
+    }
+
+    public static class Util
+    {
+        /// <summary>
+        /// Returns sum of even Fibonacci
+        /// numbers which are less than or
+        /// equal to given limit. 
+        ///  1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+        /// every third number in the Fibonacci series is even
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public static int EvenFibonacciSum(int limit)
+        {
+            if (limit < 2)
+                return 0;
+
+            var one = 2;
+            var two = 8;
+            var temp = 0;
+            var sum = 0;
+            while (one < limit)
+            {
+                //update sum 
+                sum += one;
+                // Move to next even number 
+                temp = two;
+                two = (4 * two) + one;
+                one = temp;
+            }
+            return sum;
+        }
+
+        
+        public static void Insertsort(this int[] data)
+        {
+            int n = data.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int item = data[i];
+                int ins = 0;
+                for (int j = i - 1; j >= 0 && ins != 1;)
+                {
+                    if (item < data[j])
+                    {
+                        data[j + 1] = data[j];
+                        j--;
+                        data[j + 1] = item;
+                    }
+                    else
+                    {
+                        ins = 1;
+                    }
+                }
+            }
         }
     }
 }
--- csharp/CDSPracticalTests/Tests.cs
+++ csharp/CDSPracticalTests/Tests.cs
@@ -1,6 +1,8 @@
 using CDSPractical;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CDSPracticalTests {
@@ -64,7 +66,7 @@ namespace CDSPracticalTests {
                     "homely",
                     "wiggly",
                     "thankful",
-                    "ear"
+                    "ear" 
                 })
             );
         }
@@ -74,21 +76,51 @@ namespace CDSPracticalTests {
             Assert.Equal(10.00, instance.DistanceInMiles(16.00));
         }
 
+        [Theory]
+        [InlineData(1.6, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-1.6, -1)]
+        [InlineData(10.01, 6.256)]
+        [InlineData(double.NaN, double.NaN)]
+        public void CanGetDistanceInMilesParam(double km, double expected)
+        {
+            int precision = 3;
+            var actual = instance.DistanceInMiles(km);
+            Assert.Equal(expected, actual, precision);
+        }
         [Fact]
         public void CanGetDistanceInKilometers() {
             Assert.Equal(16.00, instance.DistanceInKm(10.00));
         }
 
+        [Theory]
+        [InlineData(1,1.6)]
+        [InlineData(0, 0)]
+        [InlineData(-1,-1.6)]
+        [InlineData(6.26,10.016)]
+        [InlineData(double.NaN, double.NaN)]
+        public void CanGetDistanceInKilometersParam(double m, double expected)
+        {
+            int precision = 3;
+            var actual = instance.DistanceInKm(m);
+            Assert.Equal(expected, actual, precision);
+        }
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/Palindrome
+        /// </summary>
         [Fact]
         public void IsPalindrome() {
             var palindromes = new List<string> {
-                
-            };
-            var invalid = new List<string> {
-                
+                "madaM","raCecAr","11/11/11","Anna"
             };
 
-            foreach (var word in palindromes) {
+            var invalid = new List<string> {
+                "Hello","This is not a Palindrome"
+            };
+
+            foreach (var word in palindromes)
+            {
                 Assert.True(instance.IsPalindrome(word));
             }
 
@@ -97,19 +129,42 @@ namespace CDSPracticalTests {
             }
         }
 
-        [Fact]
-        public void CanShuffle() {
-            Assert.Equal(new List<string> { "two", "one" }, instance.Shuffle(new List<string> { "one", "two" }));
+        [Theory]
+        [InlineData("two")]
+        [InlineData("two", "one")]
+        [InlineData("two", "one","three")]
+        public void CanShuffle(params string[] input)
+        {
+            var actual = instance.Shuffle(input.ToList()).ToList();
+            //NOTE: As with less than two items, shuffle might return same result
+            if (input.Length > 2)
+            {
+                bool isEqual = actual.SequenceEqual(input);
+                Assert.False(isEqual);
+            }
+
+            //NOTE: if sorted, both should be equal. Possibly Another test
+            bool equalOrderBy = actual.OrderByDescending(x => x)
+                .SequenceEqual(input.OrderByDescending(y => y));
+            Assert.True(equalOrderBy);
         }
 
-        [Fact]
-        public void CanSort() {
-            throw new NotImplementedException();
+        [Theory]
+        [InlineData(new []{7,1,5,2,4,3,6}, new []{1,2,3,4,5,6,7})]
+        [InlineData(new[] { 7, 5,1, 2, 4, 3, 600 }, new[] { 1, 2, 3, 4, 5, 7,600 })]
+        [InlineData(new[] { int.MaxValue,1,int.MinValue }, new[] { int.MinValue,1,int.MaxValue })]
+
+        public void CanSort(int []data,int []expected) {
+            Assert.Equal(expected, instance.Sort(data));
         }
 
-        [Fact]
-        public void CanSumFibonacciNumbers() {
-            Assert.Equal(4613732, instance.FibonacciSum());
+        
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(8, 2)]
+        [InlineData(4000000, 4613732)]
+        public void CanSumFibonacciNumbers(int limit, int expected) {
+            Assert.Equal(expected, instance.FibonacciSum(limit));
         }
 
         [Fact]
@@ -121,6 +176,20 @@ namespace CDSPracticalTests {
                 Assert.Equal(current, num);
                 current++;
             }
+        }
+    }
+
+    public class MyEqualityComparer: IEqualityComparer<List<string>>
+    {
+        public bool Equals(List<string> x, List<string> y)
+        {
+            var areEquivalent = y != null && (x != null && ((x.Count == y.Count) && !x.Except(y).Any()));
+            return areEquivalent;
+        }
+
+        public int GetHashCode(List<string> obj)
+        {
+            return obj.GetHashCode();
         }
     }
 }
